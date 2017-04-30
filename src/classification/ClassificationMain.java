1189a8bf24f460ee1eed8b36fe404bc3f541d9a9
FunnyHector <hectorcaesar@hotmail.com>
2017-04-30 13:18:43 +1200

    part 3 done THIS TIME FOR REAL. Ok I forgot to test the performance on test set in last commit.

--- src/classification/ClassificationMain.java
+++ src/classification/ClassificationMain.java
@@ -7,6 +7,7 @@ import org.jgap.gp.IGPProgram;
 import org.jgap.gp.impl.DefaultGPFitnessEvaluator;
 import org.jgap.gp.impl.GPConfiguration;
 import org.jgap.gp.impl.GPGenotype;
+import org.jgap.gp.terminal.Variable;
 
 import java.util.List;
 
@@ -27,35 +28,35 @@ public class ClassificationMain {
      * @param args
      */
     public static void main(String[] args) {
-        // initialise log4j
+        // 0. initialise log4j
         PropertyConfigurator.configure("log4j.properties");
 
-        // read in all instances
+        // 1. read in all instances
         List<CancerInstance> instances = DataProcessor.readInData();
 
-        // split the instances into a training set and a test set
+        // 2. split the instances into a training set and a test set
         List<CancerInstance>[] twoLists = DataProcessor.analyseAndSplitData(instances);
         trainingSet = twoLists[0];
         testSet = twoLists[1];
 
-        // initialise a configuration object
+        // 3. initialise a configuration object
         GPConfiguration configuration = setConfiguration();
 
         try {
-            // initialise the GP problem
+            // 4. initialise the GP problem
             GPProblem problem = new ClassificationProblem(configuration);
             GPGenotype gp = problem.create();
             gp.setVerboseOutput(true);
 
-            // evolve
+            // 5. evolve
             gp.evolve(DEFAULT_NUM_EVOLUTIONS);
 
-            // show results to the console and generate the png image file
+            // 6. show results to the console and generate the png image file
             IGPProgram bestProgramme = gp.getAllTimeBest();
             gp.outputSolution(bestProgramme);
             problem.showTree(bestProgramme, "classification_tree.png");
 
-            // My own output
+            // 7. My own output
             System.out.println("====================================");
             System.out.println("The best 3 programmes:");
 
@@ -66,6 +67,14 @@ public class ClassificationMain {
                 System.out.println(" - Programme: " + program.toStringNorm(0));
                 System.out.println("   Fitness :" + program.getFitnessValue());
             });
+
+            // 8. Test the accuracy with the best solution
+            float accuracyTraining = checkAccuracy(bestProgramme, trainingSet);
+            float accuracyTest = checkAccuracy(bestProgramme, testSet);
+
+            System.out.println("====================================");
+            System.out.println(String.format("The accuracy on training set: %.2f", accuracyTraining * 100));
+            System.out.println(String.format("The accuracy on test set: %.2f", accuracyTest * 100));
 
         } catch (InvalidConfigurationException e) {
             abort(e, "Invalid Configuration Exception.");
@@ -103,6 +112,46 @@ public class ClassificationMain {
         }
 
         return config;
+    }
+
+    private static float checkAccuracy(IGPProgram gpProgram, List<CancerInstance> instances) {
+        // 9 variables
+        Variable feature1 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_1);
+        Variable feature2 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_2);
+        Variable feature3 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_3);
+        Variable feature4 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_4);
+        Variable feature5 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_5);
+        Variable feature6 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_6);
+        Variable feature7 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_7);
+        Variable feature8 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_8);
+        Variable feature9 = gpProgram.getGPConfiguration().getVariable(ClassificationProblem.FEATURE_9);
+
+        // accuracy is measured by the percentage of correctly classified instances
+        int totalNum = instances.size();
+        float numCorrect = 0.0f;
+
+        // check the performance against every instance from training set
+        for (CancerInstance cancerInstance : instances) {
+            feature1.set((float) cancerInstance.features[0]);
+            feature2.set((float) cancerInstance.features[1]);
+            feature3.set((float) cancerInstance.features[2]);
+            feature4.set((float) cancerInstance.features[3]);
+            feature5.set((float) cancerInstance.features[4]);
+            feature6.set((float) cancerInstance.features[5]);
+            feature7.set((float) cancerInstance.features[6]);
+            feature8.set((float) cancerInstance.features[7]);
+            feature9.set((float) cancerInstance.features[8]);
+
+            double result = gpProgram.execute_float(0, null);
+
+            // count how many correctly classified instances
+            if ((result >= 0 && cancerInstance.isMalignant())
+                    || (result < 0 && cancerInstance.isBenign())) {
+                numCorrect++;
+            }
+        }
+
+        return numCorrect / totalNum;
     }
 
     /**
